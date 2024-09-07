00319bfda4ae5181e18c82e8897da0813aa8aef8
Lenovo <asadbek.polatov.02@mail.ru>
2024-09-07 11:53:41 +0500

    Frontend created

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Models\Currency;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/{code?}', function ($code = null) {
+    if ($code === null) {
+        $currencies = Currency::orderby('created_date', 'ASC')
+            ->get()
+            ->groupby('ccy');
+    } else {
+        $currencies = Currency::where('ccy', $code)
+            ->orderby('created_date', 'ASC')
+            ->get()
+            ->groupby('ccy');
+    }
+    foreach ($currencies as $key => $currency) {
+        foreach ($currency as $item) {
+            $labels[] = $item['created_date'];
+            $data[$key][] = $item['rate'];
+        }
+        $color[$key][] = '#'. rand(0, 999999);
+    }
+    $labels = array_unique($labels);
+    return view('welcome', compact('labels', 'data', 'color'));
 });
