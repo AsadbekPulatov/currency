b5aa89f5925612cd93b0e19b3623105e243fcac8
Lenovo <asadbek.polatov.02@mail.ru>
2024-09-07 06:48:18 +0500

    Currency model and migration created

---
+++ database/migrations/2024_09_07_014120_create_currencies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('code');
+            $table->string('ccy');
+            $table->string('ccyNm_RU');
+            $table->string('ccyNm_UZ');
+            $table->string('ccyNm_UZC');
+            $table->string('ccyNm_EN');
+            $table->tinyInteger('nominal');
+            $table->decimal('rate');
+            $table->decimal('diff', 3);
+            $table->date('date');
+//            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('currencies');
+    }
+};
