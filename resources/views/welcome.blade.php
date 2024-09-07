cc96ba097a0e3205e61126cd18236c33e42f26f3
Lenovo <asadbek.polatov.02@mail.ru>
2024-09-07 22:38:02 +0500

    Frontend new style created

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
             display: flex;
@@ -18,13 +19,35 @@
         }
 
         canvas {
-            width: 100% !important;
+            width: 70% !important;
             height: 600px !important;
         }
     </style>
 </head>
 <body>
 <canvas id="myChart"></canvas>
+<form action="/" method="get">
+    <hr>
+    <div class="container">
+        <div class="row row-cols-5">
+            @foreach($arr_codes as $key => $item)
+                <div class="col">
+                    <label for="n{{$key}}">
+                        @if ($selected_codes == null)
+                            <input id="n{{$key}}" type="checkbox" name="codes[]" value="{{ $item }}">
+                        @else
+                            <input id="n{{$key}}" @if(in_array($item, $selected_codes)) checked @endif type="checkbox"
+                                   name="codes[]" value="{{ $item }}">
+                        @endif
+                        {{ $item }}
+                    </label>
+                </div>
+            @endforeach
+        </div>
+    </div>
+    <hr>
+    <button type="submit" class="btn btn-info w-50" style="margin-left: 50%">Filter</button>
+</form>
 </body>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
@@ -36,13 +59,13 @@
         data: {
             labels: @json($labels),
             datasets: [
-                @foreach($data as $key => $item)
-                    {
-                        label: @json($key),
-                        data: @json($item),
-                        borderWidth: 1,
-                        borderColor: @json($color[$key])
-                    },
+                    @foreach($data as $key => $item)
+                {
+                    label: @json($key),
+                    data: @json($item),
+                    borderWidth: 1,
+                    borderColor: @json($color[$key])
+                },
                 @endforeach
             ],
         },
@@ -52,11 +75,6 @@
                     beginAtZero: true
                 }
             },
-            plugins: {
-                legend: {
-                    position: 'right'
-                }
-            }
         }
     });
 </script>
--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,41 @@
 <?php
 
 use App\Models\Currency;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{code?}', function ($code = null) {
-    if ($code === null) {
-        $currencies = Currency::orderby('created_date', 'ASC')
-            ->get()
-            ->groupby('ccy');
-    } else {
-        $currencies = Currency::where('ccy', $code)
-            ->orderby('created_date', 'ASC')
-            ->get()
-            ->groupby('ccy');
+Route::get('/', function (Request $request) {
+    $selected_codes = $request->input('codes');
+    $codes = Currency::orderby('created_date', 'ASC')
+        ->get()
+        ->groupby('ccy');
+    $arr_codes = [];
+    foreach ($codes as $key => $item) {
+        $arr_codes[] = $key;
     }
+
+    $currencies = Currency::orderby('created_date', 'ASC')
+        ->get()
+        ->groupby('ccy');
+
     foreach ($currencies as $key => $currency) {
         foreach ($currency as $item) {
             $labels[] = $item['created_date'];
             $data[$key][] = $item['rate'];
         }
-        $color[$key][] = '#'. rand(0, 999999);
+        $color[$key][] = '#' . rand(0, 999999);
     }
     $labels = array_unique($labels);
-    return view('welcome', compact('labels', 'data', 'color'));
+
+    if ($selected_codes != null){
+        $new_data = [];
+        $new_color = [];
+        foreach ($selected_codes as $item){
+            $new_data[$item] = $data[$item];
+            $new_color[$item] = $color[$item];
+        }
+        $data = $new_data;
+        $color = $new_color;
+    }
+    return view('welcome', compact('labels', 'data', 'color', 'arr_codes', 'selected_codes'));
 });
